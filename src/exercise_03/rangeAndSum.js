968f72f5910a67fc4546b044fbed35e92ab35d0e
yuanqihang <qihang.yuan@thoughtworks.com>
2020-09-04 12:26:49 +0800

    完成exercise3测试

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -6,8 +6,19 @@ export function range(start, end) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  const array = [];
+  if (end >= start) {
+    // eslint-disable-next-line no-plusplus
+    for (let index = start; index < end; index++) {
+      array.push(index);
+    }
+  } else {
+    // eslint-disable-next-line no-plusplus
+    for (let index = start; index > end; index--) {
+      array.push(index);
+    }
+  }
+  return array;
 }
 
 export function sum(...numbers) {
@@ -17,6 +28,9 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let SUM = 0;
+  numbers.forEach((element) => {
+    SUM += element;
+  });
+  return SUM;
 }

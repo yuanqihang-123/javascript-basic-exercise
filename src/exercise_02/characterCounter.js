bcf6ce68b1d0ec1c8a0c5edfa8936429368b3a4e
yuanqihang <qihang.yuan@thoughtworks.com>
2020-09-04 11:58:52 +0800

    完成exercise2测试

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -6,5 +6,11 @@ export default function countCharacter(string, prediction) {
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (string === null || string === undefined) {
+    return 0;
+  }
+  if (prediction === null || prediction === undefined) {
+    return string.length;
+  }
+  return [...String(string)].filter(value => prediction(value)).length;
 }

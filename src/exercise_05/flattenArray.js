eec293a9c52695246820fda1517c034dfc29ac12
yuanqihang <qihang.yuan@thoughtworks.com>
2020-09-04 13:48:24 +0800

    完成exercise5测试

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,17 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array === null || array === undefined) {
+    throw new Error('Flatten undefined or null array');
+  }
+  let result = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] instanceof Array) {
+      result = result.concat(array[i]);
+    } else {
+      result.push(array[i]);
+    }
+  }
+  return result;
 }
